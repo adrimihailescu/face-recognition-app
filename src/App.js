28fdeb49c1c5bc050372b04d13f00ea2dca47c46
adriana <adriana.mihalescu@gmail.com>
2021-09-14 16:52:18 +0100

    setting up server, signin & register &&connecting to front-end

--- src/App.js
+++ src/App.js
@@ -40,10 +40,34 @@ class App extends Component {
       imageUrl: '',
       box: {},
       route: 'signin',
-      isSignedIn: false
+      isSignedIn: false,
+      user: {
+        id: '',
+        name: '',
+        email: '',
+        entries: 0,
+        joined: ''
+      }
     }
   }
 
+  loadUser = (data) => {
+    this.setState({
+      user: {
+        id: data.id,
+        name: data.name,
+        email: data.email,
+        entries: 0,
+        joined: new Date()
+      }
+
+    })
+  }
+  /* componentDidMount() {
+    fetch('http://localhost:3030/')
+      .then(response => response.json())
+      .then(console.log)
+  } */
 
   calculateFaceLocation = (data) => {
     const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
@@ -115,8 +139,8 @@ class App extends Component {
 
           : (
             route === 'signin'
-              ? < Signin onRouteChange={this.onRouteChange} />
-              : <Register onRouteChange={this.onRouteChange} />
+              ? <Signin onRouteChange={this.onRouteChange} />
+              : <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
           )
         }
 
