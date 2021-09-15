7697e518ebae6a0a0bf0ef7b598bef9b6ffb6ccf
adriana <adriana.mihalescu@gmail.com>
2021-09-15 11:30:07 +0100

    added how to register users and user profile update

--- src/App.js
+++ src/App.js
@@ -57,8 +57,8 @@ class App extends Component {
         id: data.id,
         name: data.name,
         email: data.email,
-        entries: 0,
-        joined: new Date()
+        entries: data.entries,
+        joined: ''
       }
 
     })
@@ -104,7 +104,23 @@ class App extends Component {
           this.state.input
         );
       })
-      .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+      .then(response => {
+        if (response) {
+          fetch('http://localhost:3030/image', {
+            method: 'put',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+              id: this.state.user.id
+            })
+          })
+            .then(response => response.json())
+            .then(count => {
+              this.setState(Object.assign(this.state.user, { entries: count }))
+            })
+
+        }
+        this.displayFaceBox(this.calculateFaceLocation(response))
+      })
       .catch(err => console.log(err));
   }
 
@@ -129,7 +145,8 @@ class App extends Component {
         {route === 'home'
           ? <div>
             <Logo />
-            <Rank />
+            <Rank name={this.state.user.name}
+              entries={this.state.user.entries} />
             <ImageLinkForm
               onInputChange={this.onInputChange}
               onButtonSubmit={this.onButtonSubmit}
@@ -139,7 +156,7 @@ class App extends Component {
 
           : (
             route === 'signin'
-              ? <Signin onRouteChange={this.onRouteChange} />
+              ? <Signin loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
               : <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
           )
         }
